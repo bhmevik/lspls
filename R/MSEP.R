6b701e89d355b58bd9836326fbab504cd417ca5b
Bjørn-Helge Mevik <b-h@mevik.net>
2006-01-06 14:50:56 +0000

    Moved creation of generic functions into .First.lib

--- R/MSEP.R
+++ R/MSEP.R
@@ -1,12 +1,6 @@
 ### MSEP.R: MSEP and RMSEP functions.
 ### $Id$
 
-## A small hack (MSEP should be made generic in pls):
-## This must go into .First.lib
-if (!exists("MSEP.default")) {
-    MSEP.default <- MSEP
-    MSEP <- function(object, ...) UseMethod("MSEP")
-}
 
 ## MSEP takes a CV-object, and calculates the MSEP
 MSEP.lsplsCv <- function(object, ...) {
@@ -15,13 +9,6 @@ MSEP.lsplsCv <- function(object, ...) {
     colMeans((object$pred - c(model.response(model.frame(object))))^2)
 }
 
-## A small hack (RMSEP should be made generic in pls, or the mvrVal
-## object should be changed to be a matrix):
-## This must go into .First.lib
-if (!exists("RMSEP.default")) {
-    RMSEP.default <- RMSEP
-    RMSEP <- function(object, ...) UseMethod("RMSEP")
-}
 
 ## RMSEP is a wrapper around MSEP that returns its square root.
 RMSEP.lsplsCv <- function(object, ...) sqrt(MSEP(object, ...))
